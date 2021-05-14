7fb4a4acb034bc70e00daa0da4063cc4c88b286b
Ivan Aleksashin <algos.inc@gmail.com>
2021-05-15 01:39:29 +0200

    update README.md, fix lint

--- README.md
+++ README.md
@@ -4,3 +4,8 @@
 [![Maintainability](https://api.codeclimate.com/v1/badges/a99a88d28ad37a79dbf6/maintainability)](https://codeclimate.com/github/codeclimate/codeclimate/maintainability)
 
 [![example workflow](https://github.com/algosinc/python-project-lvl1/actions/workflows//gh_actions.yml/badge.svg)](https://github.com/algosinc/python-project-lvl1/actions/workflows//gh_actions.yml/badge.svg)
+
+### Example 
+[![asciicast](https://asciinema.org/a/LpHXSMCuNy9D9B89lP5YVnP8W.svg)](https://asciinema.org/a/LpHXSMCuNy9D9B89lP5YVnP8W)
+
+
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -2,27 +2,29 @@
 import prompt
 import random
 
+
 def main():
     print('Welcome to the Brain Games!')
     welcome_user()
     is_number_is_even()
+
 
 def welcome_user():
     global name
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
 
+
 def is_number_is_even():
     print('Answer "yes" if the number is even, otherwise answer "no".')
-    
-    num_question_repeat = 3  #number of question repeats
-    for i in range(num_question_repeat):
-        number = random.randint(1, 100) #generate random int in range 1...100
-        is_even = number % 2 == 0  #check is num is even
 
-        answer = prompt.string(print(f'Question: {number}')) #ask question
-        #check user input
-        if answer == 'yes':
+    num_question_repeat = 3  # number of question repeats
+    for i in range(num_question_repeat):
+        number = random.randint(1, 100)  # generate random int in range 1...100
+        is_even = number % 2 == 0  # check is num is even
+
+        answer = prompt.string(print(f'Question: {number}'))  # ask question
+        if answer == 'yes':  # check user input
             is_answer_true = True
         elif answer == 'no':
             is_answer_true = False
@@ -30,17 +32,18 @@ def is_number_is_even():
             wrong_answer(answer, is_even)
             break
 
-        #check if answer is correct
+        # check if answer is correct
         if is_answer_true == is_even:
             print('Correct!')
         else:
             wrong_answer(answer, is_even)
             break
-        
-    if i == num_question_repeat-1 :
+
+    if i == num_question_repeat - 1:
         print(f"Congratulations, {name}!")
 
-def wrong_answer(answer, is_even):
+
+def wrong_answer(answer, is_even):  # wrong answer generation
     if is_even:
         print(f"'{answer}'' is wrong answer ;(. Correct answer was 'yes'.")
     else:
